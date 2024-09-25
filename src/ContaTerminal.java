a5d30f9e695848026bdf8dd2949a2a1879b0e7d2
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2024-09-25 13:55:33 -0300

    feat: implement ContaTerminal class for bank account creation

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.next();
+        
+        System.out.println("Por favor, digite o número da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o valor do saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.", nomeCliente, agencia, conta, saldo);
+
+        scanner.close();
     }
 }
